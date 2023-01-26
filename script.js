1a21afaa31e01bbfde45f4376d8eb6bb0ff05cc2
Gabriel Oliveira <gabriel.sv.oliver@gmail.com>
2023-01-26 20:25:03 -0300

    Limitando area do BtnMove

--- script.js
+++ script.js
@@ -5,24 +5,14 @@ const h1Change = document.querySelector("h1");
 button.addEventListener("mouseover", moveRandomly);
 
 function moveRandomly() {
-  let x = Math.floor(Math.random() * window.innerWidth);
-  let y = Math.floor(Math.random() * window.innerHeight);
-  const width = button.offsetWidth;
-  const height = button.offsetHeight;
-  const limitX = window.innerWidth - width;
-  const limitY = window.innerHeight - height;
-  while (x < 0 || x > limitX) {
-      x = Math.floor(Math.random() * window.innerWidth);
-  }
-  while (y < 0 || y > limitY) {
-      y = Math.floor(Math.random() * window.innerHeight);
-  }
-  button.style.transform = `translate(${x}px, ${y}px)`;
+let x = Math.floor(Math.random() * 400);
+let y = Math.floor(Math.random() * 400);
+button.style.transform = `translate(${x}px, ${y}px)`;
 }
 
 btnYes.addEventListener("click", updateH1);
 
 function updateH1(){
-    h1Change.textContent = "Eu sempre soube que você era!";
+h1Change.textContent = "Eu sempre soube que você era!";
 }
 
